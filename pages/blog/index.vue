38e29c0008e2fda91f84a12628f84dc6eb1ab60a
Benoit Aspirault <benoit.aspirault@gmail.com>
2020-02-26 00:44:53 -0500

    added use of fluidGrid Utility

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="previewWrapper">
+  <div class="fluidGrid">
     <div v-for="(post, index) of posts" :key="index">
       <post-preview prefix="blog" :slug="slug" :post="posts[index]" />
     </div>
@@ -30,11 +30,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.previewWrapper {
-  padding: 16px 16px 16px 16px;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(308px, 1fr));
-  gap: 16px;
-}
-</style>
+<style scoped></style>
